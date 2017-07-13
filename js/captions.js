4efe0132fb19c948128a07a3471aed7775dda4bf
Shubhang Sati <shubhang16198@iiitd.ac.in>
2017-07-14 03:07:22 +0530

    captions added

--- js/captions.js
+++ js/captions.js
@@ -1,17 +1,29 @@
 function showcaption(id) {
+    var a = document.getElementById(id).offsetHeight;
     document.getElementById(id).style.bottom = "0px";
 }
 function hidecaption(id) {
     var a = document.getElementById(id).offsetHeight;
-    document.getElementById(id).style.bottom = "-" + a + "px";
+    document.getElementById(id).style.bottom = "-" + (a - 60) + "px";
 }
 $(document).ready(function() {
     var a = document.getElementById("captionpanel1").offsetHeight;
-    document.getElementById("captionpanel1").style.bottom = "-" + a + "px";
+    document.getElementById("captionpanel1").style.bottom = "-" + (a - 60) + "px";
     var a = document.getElementById("captionpanel2").offsetHeight;
-    document.getElementById("captionpanel2").style.bottom = "-" + a + "px";
+    document.getElementById("captionpanel2").style.bottom = "-" + (a - 60) + "px";
     var a = document.getElementById("captionpanel3").offsetHeight;
-    document.getElementById("captionpanel3").style.bottom = "-" + a + "px";
+    document.getElementById("captionpanel3").style.bottom = "-" + (a - 60) + "px";
     var a = document.getElementById("captionpanel4").offsetHeight;
-    document.getElementById("captionpanel4").style.bottom = "-" + a + "px";
+    document.getElementById("captionpanel4").style.bottom = "-" + (a - 60) + "px";
+})
+
+$(window).resize(function() {
+    var a = document.getElementById("captionpanel1").offsetHeight;
+    document.getElementById("captionpanel1").style.bottom = "-" + (a - 60) + "px";
+    var a = document.getElementById("captionpanel2").offsetHeight;
+    document.getElementById("captionpanel2").style.bottom = "-" + (a - 60) + "px";
+    var a = document.getElementById("captionpanel3").offsetHeight;
+    document.getElementById("captionpanel3").style.bottom = "-" + (a - 60) + "px";
+    var a = document.getElementById("captionpanel4").offsetHeight;
+    document.getElementById("captionpanel4").style.bottom = "-" + (a - 60) + "px";
 })
